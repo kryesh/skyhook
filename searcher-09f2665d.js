b1155241c6f5bc2e3c5ab0fcf042acf2ed898fd5
Kryesh <kryesh@tuta.com>
2026-09-26 20:21:25 +1000

    Publish documentation from 9f711a5643774f7a4238ca1e24afe0b548c37486

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-393d7e39.js',
+                path_to_root + 'searchindex-3c76d1f2.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
